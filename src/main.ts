fb574c0c2bc3e901cc0b6c900c043e0880f4888b
horan <13571899655@163.com>
2026-01-06 17:34:41 +0800

    feat: optimize

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
 import './assets/main.css'
 
 import { ViteSSG } from 'vite-ssg'
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-/* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 
 import App from './App.vue'
-/* add icons to the library */
-library.add(faUserSecret)
 
 const routes = [
     { path: '/', component: () => import('./pages/Home.vue') },
@@ -22,7 +14,4 @@ const routes = [
 export const createApp = ViteSSG(
     App,
     { routes },
-    ({ app }) => {
-        app.component('font-awesome-icon', FontAwesomeIcon)
-    },
 )
